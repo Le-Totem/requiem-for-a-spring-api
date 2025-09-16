a4c8f71b4ca4077f6ff62673dd1027ed02a0ff8b
imulcei <lucie.fiorrito@proton.me>
2025-09-16 16:54:09 +0200

    Initialisation du projet

---
+++ src/main/java/fr/afpa/requiem_of_a_spring/RequiemOfASpringApplication.java
@@ -0,0 +1,13 @@
+package fr.afpa.requiem_of_a_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequiemOfASpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequiemOfASpringApplication.class, args);
+		System.out.println("Hello");
+	}
+}
---
+++ src/test/java/fr/afpa/requiem_of_a_spring/RequiemOfASpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.afpa.requiem_of_a_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequiemOfASpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
