a93b26b0500761cbc73481036821aa9f859b909d
imulcei <lucie.fiorrito@proton.me>
2025-09-17 15:39:09 +0200

    ajout JPA dans pom.xml

---
+++ src/main/java/fr/afpa/requiem_for_a_spring/entities/MusicPiece.java
@@ -0,0 +1,10 @@
+package fr.afpa.requiem_for_a_spring.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "music_piece")
+public class MusicPiece {
+
+}
