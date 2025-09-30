b97c577a0d39f88d4d229b2257fc7477bcb329d0
imulcei <lucie.fiorrito@proton.me>
2025-09-30 14:00:15 +0200

    email unique OK

--- DB/dump/ScriptInitialisation.sql
+++ DB/dump/ScriptInitialisation.sql
@@ -6,7 +6,7 @@ CREATE TABLE "user"(
    id UUID,
    firstname VARCHAR(50)  NOT NULL,
    lastname VARCHAR(50)  NOT NULL,
-   email VARCHAR(50)  NOT NULL,
+   email VARCHAR(50) UNIQUE NOT NULL,
    "password" VARCHAR(255)  NOT NULL,
    is_validated BOOLEAN NOT NULL,
    picture VARCHAR(150) ,
